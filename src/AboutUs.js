aef3ce2df6ef8cd295240310babced7c09231fae
charitha261101 <95222423+charitha261101@users.noreply.github.com>
2023-11-07 14:07:32 +0530

    Delete src directory

--- src/AboutUs.js
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-
-function AboutUs()
-{
-    return(
-        <Typography variant="h4" align="center">About Us</Typography>
-    )
-}
-export default AboutUs;
